95988677f603d244929f6c4544b6bf07b9ad44b8
imamfarisi19 <edwardindo1@gmail.com>
2022-06-23 15:45:55 +0800

    model,seed,factories,controller created

---
+++ database/migrations/2022_06_23_074406_create_tempat_wisatas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tempat_wisatas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tempat_wisatas');
+    }
+};
